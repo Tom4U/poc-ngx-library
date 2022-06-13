9ec9fa1245ea95defbd1d47b713deadeef9576bf
Tom <tom@ohms.pro>
2022-06-13 16:18:29 +0200

    feat(app): add header, dummy list and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngx-library';
+  title = 'Meine kleine Bibliothek';
+  year = new Date().getFullYear();
 }
